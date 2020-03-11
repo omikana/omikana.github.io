9ab3086deadf7b83ad87a69faae5a23963bd7fb1
omikana0123 <0123kana0123@gmail.com>
2020-03-11 10:50:32 +0900

    spでの動作確認

---
+++ sp.js
@@ -0,0 +1,16 @@
+function menu() {
+  document.getElementById(spImgMenu).innerHTML ='<div class="spmenujs sp">
+  <ul class="row">
+  <li class="col"><img src="imgsp/profile.png" alt="プロフィール"></li>
+  <li class="col"><img src="imgsp/skill.png" alt="スキル"></li>
+  <li class="col"><img src="imgsp/gallery.png" alt="作品集"></li>
+  <li class="col"><img src="imgsp/github.png" alt="ギットハブ"></li>
+  <li class="col"><img src="imgsp/menu_sp.png" alt="メニュー"></li>
+  <li class="col"><img src="imgsp/form.png" alt="お問合せ"></li>
+  <li class="col">7</li>
+  <li class="col">8</li>
+  <li class="col">9</li>
+</ul>
+</div>
+';
+}
