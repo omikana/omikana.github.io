3d000db54a5dce9a42dd786f68605d4bb7906be0
omikana0123 <0123kana0123@gmail.com>
2020-03-12 23:58:32 +0900

    修正　途中経過

--- sp.js
+++ sp.js
@@ -1,16 +1,3 @@
 function menu() {
-  document.getElementById(spImgMenu).innerHTML ='<div class="spmenujs sp">
-  <ul class="row">
-  <li class="col"><img src="imgsp/profile.png" alt="プロフィール"></li>
-  <li class="col"><img src="imgsp/skill.png" alt="スキル"></li>
-  <li class="col"><img src="imgsp/gallery.png" alt="作品集"></li>
-  <li class="col"><img src="imgsp/github.png" alt="ギットハブ"></li>
-  <li class="col"><img src="imgsp/menu_sp.png" alt="メニュー"></li>
-  <li class="col"><img src="imgsp/form.png" alt="お問合せ"></li>
-  <li class="col">7</li>
-  <li class="col">8</li>
-  <li class="col">9</li>
-</ul>
-</div>
-';
+  document.getElementById("spImgMenu").innerHTML =' <div class="spmenujs"><ul class="row"><li class="col"><a href="profile.html"><img src="imgsp/profile.png" alt="プロフィール"></a></li><li class="col"><a href="skill.html"><img src="imgsp/skill.png" alt="スキル"></a></li><li class="col"><a href="gallery.html"><img src="imgsp/gallery.png" alt="作品集"></a></li><li class="col"><a href="#"><img src="imgsp/github.png" alt="ギットハブ"></a></li><li class="col"><a href="#"><img src="imgsp/menu_sp.png" alt="メニュー"></a></li><li class="col"><a href="#"><img src="imgsp/form.png" alt="お問合せ"></a></li><li class="col"><a href="#">7</a></li><li class="col"><a href="#">8</a></li><li class="col"><a href="#">9</a></li></ul></div> ';
 }
